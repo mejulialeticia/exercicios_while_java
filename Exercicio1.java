c70bc9da5594e56b400b1a450547994ef60b8603
Julia <151403603+mejulialeticia@users.noreply.github.com>
2024-02-29 23:08:40 +0000

    contgem regressiva de 100 a 1

---
+++ Exercicio1.java
@@ -0,0 +1,9 @@
+public class Exercicio1 {
+    public static void main(String[] args) {
+        int contador = 100; 
+        while(contador > 0){
+            System.out.println("numero: " + contador);
+            contador --;
+        }
+    }
+}
