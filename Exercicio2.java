4e385bb3cb1c6596c9d412bc20372ecdac00f794
Julia <151403603+mejulialeticia@users.noreply.github.com>
2024-02-29 23:12:19 +0000

    contagem crescente de 1 a 100

---
+++ Exercicio2.java
@@ -0,0 +1,9 @@
+public class Exercicio2 {
+    public static void main(String[] args) {
+        int contador = 0; 
+        while(contador < 101){
+            System.out.println("numero: " + contador);
+            contador ++;
+        }
+    }
+}
